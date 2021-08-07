9e9e88a5624b4722cb2f11253e37cf8503b06fcf
akshobhya0 <81753694+akshobhya0@users.noreply.github.com>
2021-08-07 14:44:58 +0530

    Add files via upload

---
+++ Iron.js
@@ -0,0 +1,8 @@
+class Iron{
+	constructor(x,y,r)
+}
+var option={
+    "restituion":0.8,
+    "friction":3,
+    "density":30 
+}
---
+++ Stone.js
@@ -0,0 +1,9 @@
+class Stone{
+	constructor(x,y,r)
+}
+
+var option= {
+    "restitution" : 0.8,
+    "friction":0.9,
+    "density": 12 
+  } 
